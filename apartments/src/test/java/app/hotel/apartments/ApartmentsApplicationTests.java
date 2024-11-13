9c2ea08cf5bceb7dd82043342af4230dda6276b6
ikazerskiy <Ilya.Kazerskiy@artezio.com>
2024-11-13 14:27:19 +0300

    init project

---
+++ apartments/src/test/java/app/hotel/apartments/ApartmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package app.hotel.apartments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
